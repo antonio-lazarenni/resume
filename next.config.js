a472359b782129429e8ab9bc32467feb2e5d405b
Anton Lazarenko <anton.lazarenko@ibanwallet.com>
2021-01-29 17:09:39 +0000

    lang attr added to the page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
     images: {
         domains: ['media.graphcms.com']
     },
+    i18n: {
+        locales: ['en-US'],
+        defaultLocale: 'en-US'
+    },
     async rewrites() {
         return [
             {
